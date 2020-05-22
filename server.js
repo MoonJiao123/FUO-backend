8014635a41345edd17096b48d588dd6ea6bdc619
mrtiggles5 <dta858king@gmail.com>
2020-05-22 12:09:21 -0700

    Added include for the session, will need to be installed in the npm

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ var cors = require('cors') // handle the cors domain requests
 var bodyParser = require('body-parser') //allow us to extract the data sent from the FE
 var app = express()
 var path = require('path')
+
+var session = require('express-session')
+
 const port = process.env.PORT || 5000;
 
 //app.use mounts the middleware function at a specific path
