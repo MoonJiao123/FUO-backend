cc7141fde5405939b4d1bdca2e7553f744172726
MoonJiao123 <yuj001@ucsd.edu>
2020-05-26 23:12:38 -0700

    https true

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(session({
   secret: SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: true}
+  cookie: { secure: true, httpOnly: true}
 })); 
 
 //parse the data with json, the query string library
