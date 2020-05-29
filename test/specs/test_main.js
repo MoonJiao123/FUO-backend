f489dcd4d31a2d89a77e828cce2786af97fbbbb0
Shawn <sjkoo@ucsd.edu>
2020-05-28 23:45:31 -0700

    added some tests

--- test/specs/test_main.js
+++ test/specs/test_main.js
@@ -6,17 +6,22 @@
 * https://webdriver.io/docs/sync-vs-async.html
 * https://webdriver.io/blog/2019/04/03/react-selectors.html
 *
+* Examples of React webdriver.io test
+* https://github.com/klamping/react-webdriverio-tests/blob/master/test/input-change-events.test.js
+*
 * READ INSTRUCTIONS TO EXECUTE TESTS
 * ----------------------------------
 * 1) Locate to the home directory of fuo-backend
 * 2) From the root directory of fuo-backend, execute the command "npx wdio wdio.conf.js"
 *
-* Contributors: Derek Ta
+* Contributors: Derek Ta, Shawn, Jeet
 */
 
 /*Main Test - Just verifies the main page loads correctly*/
 /*This is a redundant test for the purpose of showing how to start writing tests*/
 const TEAM_HEROKU_URL = 'https://corona-food.herokuapp.com';
+
+
 
 describe('test_main', () => {
     it('should have the right title', () => {
@@ -24,5 +29,103 @@ describe('test_main', () => {
         browser.url(TEAM_HEROKU_URL)
         const title = browser.getTitle()
         expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
+    })
+    it('Main page should have correct Title', () => {
+        /* Opens the browser URL */
+        browser.url(TEAM_HEROKU_URL)
+        const title = browser.getTitle()
+        expect(browser).toHaveTitle('Stop Food Waste');
+    })
+
+    it('Tab', () => {
+        /* Opens the browser URL*/
+        browser.url(TEAM_HEROKU_URL);
+        const myButton = $('#simple-tab-1');
+        myButton.click()
+
+    })
+
+    it('The back button should go to landing page', () => {
+        /*Opens the browser URL*/
+        browser.url(TEAM_HEROKU_URL);
+        const myButton = $('#accounts-back');
+        myButton.click();
+        expect(browser).toHaveUrl('https://corona-food.herokuapp.com/landing.html%27');
+    })
+
+    it('The back button should go to lane', () => {
+        /*Opens the browser URL*/
+        browser.url(TEAM_HEROKU_URL);
+        const toggleButton = $('button *= Sign Up');
+
+    })
+})
+/* Testing login and logout - Shawn */
+describe('Authentication Test Suite', function(){
+    it('should click on the login button and login form presented', function() {
+      return browser
+        .url(TEAM_HEROKU_URL)
+        .click('#login')
+        .getText('#login-form-submit')
+        .should.eventually.be.equal('LOGIN');
+    });
+
+  it('should click on the login button submit login info and a logout link is visible', function() {
+    return browser
+      .url(TEAM_HEROKU_URL)
+      .setValue('#email', 'sun@y.com')
+      .setValue('#password', '12345')
+      .click('#login-form-submit')
+      .waitForVisible('#logout', 3000)
+      .getText('#logout')
+      .should.eventually.be.equal('Logout');
+  });
+
+  it('should logout when logout link is clicked', function() {
+    return browser
+      .url(TEAM_HEROKU_URL)
+      .getText('#logout-text')
+      .should.eventually
+      .be.equal("You are now logged out. Thank you for visiting.");
+  });
+
+});
+
+//Testing if pop up for add location pops up -Shawn
+describe('test_add_location', () => {
+    /* Testing ADD LOCATION button */
+    it('expect add a location pop up', () => {
+        /*Opens the browser URL*/
+        browser.url('http://localhost:3000/business');
+        const myButton = $('button *= ADD LOCATION');
+        myButton.click();
+        // assertion to be TBD
+    })
+
+    /* Testing LOG OUT button */
+    it('logout', () => {
+        /*Opens the browser URL*/
+        browser.url('http://localhost:3000/business');
+        const myButton = $('button *= LOG OUT');
+        myButton.click();
+        // assertion to be TBD
+    })
+
+    /* Testing UPDATE LIST button */
+    it('logout', () => {
+        /*Opens the browser URL*/
+        browser.url('http://localhost:3000/business');
+        const myButton = $('button *= UPDATE LOCATION');
+        myButton.click();
+        // assertion to be TBD
+    })
+
+    /* Testing DELETE LOCATION button */
+    it('logout', () => {
+        /*Opens the browser URL*/
+        browser.url('http://localhost:3000/business');
+        const myButton = $('button *= DELETE LOCATION');
+        myButton.click();
+        // assertion to be TBD
     })
 })
